bb618b08d904d1ae2a8cee8523740a3bcebaa68c
oliver <oliverdeleong@gmail.com>
2022-09-03 03:13:04 -0400

    improve login page animations

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import DynamicFeedOutlinedIcon from '@mui/icons-material/DynamicFeedOutlined';
 import LayersOutlinedIcon from '@mui/icons-material/LayersOutlined';
 import NoteOutlinedIcon from '@mui/icons-material/NoteOutlined';
@@ -9,11 +9,47 @@ function Login() {
 
   const [headingVisible, setHeadingVisible] = useState(false);
   const [animate, setAnimate] = useState({
-    leftSide: false
+    icon1: false,
+    icon2: false,
+    icon3: false
   });
 
+  const [animateOut, setAnimateOut] = useState({
+    leftSide: false,
+    icon1: false,
+    icon2: false,
+    icon3: false
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeout(() => {
+        setAnimate({
+          icon1: true,
+          icon2: false,
+          icon3: false
+        });
+        setTimeout(() => {
+          setAnimate({
+            icon1: true,
+            icon2: true,
+            icon3: false
+          });
+          setTimeout(() => {
+            setAnimate({
+              icon1: true,
+              icon2: true,
+              icon3: true
+            })
+          }, 600);
+        }, 600);
+      }, 600);
+    }, 500);
+    
+  }, []);
+
   const login = () =>{
-    setAnimate({leftSide: !animate.leftSide});
+    setAnimateOut({...animateOut, leftSide: !animateOut.leftSide});
     setTimeout(() => {
       setHeadingVisible(!headingVisible);
     }, 1500);
@@ -21,25 +57,32 @@ function Login() {
 
   return (
     <div className='loginContainer'>
-      <div className= {`loginPageLeftSide ${animate.leftSide ? 'animateLogin': ''}`}>
+      <div className= {`loginPageLeftSide ${animateOut.leftSide ? 'animateLogin': ''}`}>
         
         <div className='heading1 slideUp'><h1>Flash<span style={{color: "#141259"}}>Dex</span></h1></div>
 
         <div className='icons'>
-          <div className='iconContainer slideUp'>
-            <LayersOutlinedIcon className='icon'/>
-            <h2>Create decks</h2>
-          </div>
+          {animate.icon1 && (
+            <div className='iconContainer slideUp'>
+              <LayersOutlinedIcon className='icon'/>
+              <h2>Create decks</h2>
+            </div>
+          )}
           
-          <div className='iconContainer slideUp'>
-            <NoteOutlinedIcon className='icon' />
-            <h2>Fill in cards</h2>
-          </div>
+          {animate.icon2 && (
+            <div className='iconContainer slideUp'>
+              <NoteOutlinedIcon className='icon' />
+              <h2>Fill in cards</h2>
+            </div>
+          )}
 
-          <div className='iconContainer slideUp'>
-            <ListAltOutlinedIcon className='icon' />
-            <h2>Study efficiently</h2>
-          </div>
+          {animate.icon3 && (
+            <div className='iconContainer slideUp'>
+              <ListAltOutlinedIcon className='icon' />
+              <h2>Study efficiently</h2>
+            </div>
+          )}
+          
         </div>
         
         
