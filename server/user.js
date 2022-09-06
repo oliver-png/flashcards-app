0fb083ff19c3d57794ad311d1ab64901f922549f
oliver <oliverdeleong@gmail.com>
2022-09-06 01:21:11 -0400

    add database schemas

---
+++ server/user.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+
+  answer: {
+    type: String,
+    required: true
+  }
+});
+
+const deckSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  cards: [cardSchema],
+  dateCreated: String
+});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  decks: [deckSchema]
+});
+
+module.exports = mongoose.model("User", userSchema);
