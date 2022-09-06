8fc44e4d3f9ee96a423123400e0b84c56b857e24
oliver <oliverdeleong@gmail.com>
2022-09-06 01:13:52 -0400

    add individual deck route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Registration from "./pages/Registration";
+import Deck from "./pages/Deck";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Registration />} />
+          <Route path="/deck" element={<Deck />} />
         </Routes>
     </Router>
     
